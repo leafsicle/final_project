a1629df203d04951b33c8f330839f25af1afcb49
Matt Cooke <cookem529@gmail.com>
2018-10-30 08:33:54 -0400

    made changes to app and initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './Header'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class="container">
+				<Header />
+				{/* this will be the main area for the app. see drawing 1 in notebook for now. */}
+				{/* I needed to add code for my commit */}
+				{/* On the main page I will have the overall difference,
+          the income, and the expenses. The bills will be low as roughly follows*/}
+				<body>
+					<section class="section">
+						<div class="container">
+							<h1 class="title">Hello World</h1>
+							<p class="subtitle" />
+						</div>
+					</section>
+				</body>
+				{/* <div class="main">
+					<p class="diff">+/- $XXX</p>
+					<p class="income">$xxx</p>
+					<p class="expenses">$xxx</p>
+				</div> */}
+				{/* In this section tall of the bills and 
+      income will be listed with income on top and expenses 
+      listed below */}
+				<table />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+	render() {
+		return (
+			<div>
+				<p class="thisMonth">Current Month</p>
+				<h1 class="title">Balanced Budget</h1>
+				<h2>Spare the Expenses</h2>
+			</div>
+		)
+	}
+}
+
+export default Header
